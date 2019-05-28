74d4991a7eae12408a68a7902c70608af81dc48f
Zach Butler <zach.standley93@gmail.com>
2019-05-28 16:46:28 -0700

    styled avatar-list

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
                 </header>
 
                 <main>
-                    <ul>
+                    <ul class="avatar-list">
                         <li>
                             <h2>46th Earth King</h2>
                             <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" alt="46th Earth King">
