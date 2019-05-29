a0ed4d526b25d8807484874fbddaf0aa5fe143d7
Zach Butler <zach.standley93@gmail.com>
2019-05-28 17:02:50 -0700

    made avatar list component than linked to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
 import Component from './Component.js';
+import AvatarList from './AvatarList.js';
 
 class App extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const main = dom.querySelector('main');
+
+        const avatarList = new AvatarList();
+        main.appendChild(avatarList.render());
+
+        return dom;
+    }
+
     renderTemplate() {
         return /*html*/`
             <div>
@@ -12,20 +24,7 @@ class App extends Component {
                 </header>
 
                 <main>
-                    <ul class="avatar-list">
-                        <li class="avatar-item">
-                            <h2>46th Earth King</h2>
-                            <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" alt="46th Earth King">
-                        </li>
-                        <li class="avatar-item">
-                            <h2>Aang</h2>
-                            <img src="https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003" alt="Aang">
-                        </li>
-                        <li class="avatar-item">
-                            <h2>Aang (games)</h2>
-                            <img src="https://vignette.wikia.nocookie.net/avatar/images/2/21/Into_The_Inferno_Aang.png/revision/latest?cb=20131009060746" alt="Aang (games)">
-                        </li>
-                    </ul>
+                    
                 </main>
             </div>
         `;
