74770f73c3969e3d59e9b44353ccb145d21c11a6
Zach Butler <zach.standley93@gmail.com>
2019-05-29 13:25:02 -0700

    fixed travis error in test

--- test/AvatarItem.test.js
+++ test/AvatarItem.test.js
@@ -4,14 +4,14 @@ const test = QUnit.test;
 
 test('renders template from data', function(assert) {
     //Arrange
-    const avatar = {
+    const character = {
         _id: '5cdf0769b6e02a467e3e766b',
         photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441',
         name: '46th Earth King'
     };
 
     //Act 
-    const avatarItem = new AvatarItem({ avatar });
+    const avatarItem = new AvatarItem({ character });
     const rendered = avatarItem.renderTemplate();
     //Assert
     assert.htmlEqual(rendered, /*html*/`
