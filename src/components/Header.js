668c5f3e3de9939d782f4420188c89ad71bc0b47
Zach Butler <zach.standley93@gmail.com>
2019-05-29 17:17:21 -0700

    added home button and styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,13 @@ class Header extends Component {
     renderTemplate() {
         return /*html*/`
             <header>
-                <img src="../assets/elements.png">
-                <h1>
-                    Avatar
-                </h1>
+                <div>
+                    <img src="../assets/elements.png">
+                    <h1>
+                        Avatar
+                    </h1>
+                </div>
+                <a href="index.html">Home</a>
             </header>
         `;
     }
