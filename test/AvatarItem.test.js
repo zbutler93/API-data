cfcc59ce3496744b4b1fd3194a6b0cc6b7104ef5
Zach Butler <zach.standley93@gmail.com>
2019-05-31 16:17:30 -0700

    fixed travis error

--- test/AvatarItem.test.js
+++ test/AvatarItem.test.js
@@ -18,8 +18,8 @@ test('renders template from data', function(assert) {
         <li class="avatar-item">
             <h2>46th Earth King</h2>
             <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" alt="46th Earth King">
-            <a href=?allies=46th Earth King>allies</a>
-            <a href=?enemies=46th Earth King>enemies</a>
+            <a href=#allies=46th Earth King>allies</a>
+            <a href=#enemies=46th Earth King>enemies</a>
         </li>
     `);
 });
