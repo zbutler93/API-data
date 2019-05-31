36613d38fd65776f1bd21ef06c242b136e410056
Zach Butler <zach.standley93@gmail.com>
2019-05-31 14:58:02 -0700

    appended search component to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
 import Component from './Component.js';
+import Search from './Search.js';
 
 class Header extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const search = new Search();
+        dom.appendChild(search.render());
+
+        return dom;
+    }
     
     renderTemplate() {
         return /*html*/`
